ca5a3351bc68d9ac9c30f26b00368ca4487b43e5
Gautier de Lataillade <gautier2406@gmail.com>
2019-04-30 15:45:06 +0200

    Add flag parsing

--- include/chap.h
+++ include/chap.h
@@ -11,8 +11,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <getopt.h>
+#include <stdio.h>
 
 // Structures:
+struct option long_options[] = {
+    {"target", required_argument, NULL, 't'},
+    {"port", required_argument, NULL, 'p'},
+    {"password", required_argument, NULL, 'P'},
+    {0, 0, 0, 0}
+};
+
+struct client_s
+{
+    char *target;
+    int port;
+    char *password;
+};
 
 // Prototypes:
 
--- src/main.c
+++ src/main.c
@@ -7,8 +7,43 @@
 
 #include "chap.h"
 
-int main(int argc, char const *argv[])
+struct client_s init_struct(void)
 {
-    /* code */
-    return 84;
+    struct client_s client;
+
+    client.password = NULL;
+    client.port = -1;
+    client.target = NULL;
+    return client;
+}
+
+int main(int argc, char *argv[])
+{
+    int option_index = 0;
+    int opt;
+    struct client_s client = init_struct();
+
+    while (1) {
+        opt = getopt_long(argc, argv, "t:p:P:", long_options, &option_index);
+        if (opt == -1)
+            break;
+        switch(opt) {
+            case 't':
+                client.target = optarg;
+                break;
+            case 'p':
+                client.port = atoi(optarg);
+                break;
+            case 'P':
+                client.password = optarg;
+                break;
+            default:
+                puts("Wrong args\n");
+                exit(1);
+        }
+    }
+    printf("target: %s\n", client.target);
+    printf("port: %d\n", client.port);
+    printf("password: %s\n", client.password);
+    return 0;
 }
